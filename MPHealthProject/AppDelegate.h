b52bfc2755993fe35ae4501c4533ad33a336c394
plum <821334454@qq.com>
2017-06-17 14:41:13 +0800

    Initial Commit

---
+++ MPHealthProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MPHealthProject
+//
+//  Created by plum on 17/6/17.
+//  Copyright © 2017年 plum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
